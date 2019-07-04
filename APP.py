1e1d76cb0823559c1f5df595b069345d4654fbbc
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-04 13:12:06 +0200

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,7 @@
+class frac:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def mult(self, other):
+        return frac(self.a*other.a, self.b*other.b)
---
+++ TEST.py
@@ -0,0 +1,10 @@
+import APP
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(frac(1, 2).mult(frac(2, 3)).a, 3)
+
+
+
